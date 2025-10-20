ba395cbaece602dd5d8271b27893589331af4f0f
hohohoho1223 <dnjsgh1820@naver.com>
2025-10-20 20:41:16 +0900

    feat: 계산기 애플리케이션 실행 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,12 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorController controller = new CalculatorController(
+                new InputView(),
+                new OutputView(),
+                new StringCalculator(),
+                new DelimiterParser()
+        );
+        controller.run();  // 프로그램 흐름 시작
     }
 }
